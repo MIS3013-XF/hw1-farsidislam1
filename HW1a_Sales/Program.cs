1afc8810afcc5929ccb95d8bf7422f6f2bd8e61c
Farsid Islam <farsid@ou.edu>
2025-09-08 12:34:37 -0500

    I stated all my variables and created my formulas

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,11 +1,13 @@
 ﻿// HW1a Sales Total
 
 // Your Name: Farsid Islam
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Did you seek help ? If yes, specify the helper or web link here: I used the slides provided in class to help me.
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.Remoting.Metadata.W3cXsd2001;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,7 +17,23 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helloooooooo");
+
+            Console.WriteLine("What kind of ticket do you wanna buy?");
+            string myTicket = Console.ReadLine();
+
+            Console.WriteLine("How many would you like to buy?");
+            int myAmount = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What is the price?");
+            double myPrice = Convert.ToDouble(Console.ReadLine());
+
+            double AmtXPrice = myAmount * myPrice;
+            double salesTax = AmtXPrice * 0.085;
+            double total = AmtXPrice + salesTax;
+
+            Console.WriteLine($"Your subtotal is {AmtXPrice}");
+            Console.WriteLine($"Your sales tax is {salesTax}");
+            Console.WriteLine($"Your final total is {total}");
         }
     }
 }
